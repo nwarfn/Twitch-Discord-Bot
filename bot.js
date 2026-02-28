3e03b1144c67fc42c1ff27e296acfb6c015d2c62
Nwar <alexfn761@gmail.com>
2026-02-28 20:32:14 +0300

    Create bot.js
    Bot js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const config = require('./config.js');
+const fs = require('fs');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
+    }
+});
+
+client.login(config.token);
